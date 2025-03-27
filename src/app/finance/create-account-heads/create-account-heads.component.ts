e2a59613110d2a9a95a222553cf5d4a8718ff6c1
arjuntechpool <arjun@techpool.co.in>
2025-03-27 13:55:16 +0530

    Display Table Values

--- src/app/finance/create-account-heads/create-account-heads.component.ts
+++ src/app/finance/create-account-heads/create-account-heads.component.ts
@@ -3,54 +3,75 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { SearchSecondaryHeadsComponent } from '../modals/search-secondary-heads/search-secondary-heads.component';
+import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-create-account-heads',
   templateUrl: './create-account-heads.component.html',
-  styleUrls: ['./create-account-heads.component.scss']
+  styleUrls: ['./create-account-heads.component.scss'],
 })
-
-
-
 export class CreateAccountHeadsComponent implements OnInit {
+  selected_acc_head = {
+    parent_head: '',
+    head_code: '',
+    head: '',
+    short_description: '',
+    primary: '',
+    secondary: '',
+  };
+  type: any;
+  head_list: any = [];
 
-  
-
-  SubjectData:any = [
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    { type: 'Income', code: '1000001', head: 'Sales (Revenue Account)'},
-    
-  ];
-
-  
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   displayedColumns: string[] = ['type', 'code', 'head'];
-  dataSource = new MatTableDataSource<any>;
+  dataSource = new MatTableDataSource<any>();
 
+  constructor(private dialog: MatDialog, private svr: ServiceService) {}
 
-  constructor(
-        private dialog: MatDialog
-    
-  ) { }
   ngOnInit(): void {
     // Initialize paginator
-    this.dataSource = new MatTableDataSource(this.SubjectData);
+    this.fetch_heads();
     this.dataSource.paginator = this.paginator;
   }
 
-
-  open_heads(){
-      const dialogRef = this.dialog.open(SearchSecondaryHeadsComponent, {
-          width: '1130px',
-        });
-        dialogRef?.afterClosed().subscribe((response: any) => {
-
-        });
+  open_heads() {
+    const dialogRef = this.dialog.open(SearchSecondaryHeadsComponent, {
+      width: '1130px',
+    });
+    dialogRef?.afterClosed().subscribe((response: any) => {
+      if (response && response.data) {
+        const userData = response.data;
+        this.selected_acc_head = {
+          parent_head: userData.vch_primary_head,  // Map to vch_primary_head
+          head_code: userData.vch_secondary_code,  // Map to vch_secondary_code
+          head: userData.vch_secondary_head,       // Map to vch_secondary_head
+          short_description: "",                   // No equivalent in API, set as empty
+          primary: userData.int_primary_id,        // Map to int_primary_id
+          secondary: userData.int_secondary_id,    // Map to int_secondary_id
+        };
+        console.log('Selected Acc Head:', this.selected_acc_head);
+      }
+    });
   }
 
+
+  // Fetch data from API
+  fetch_heads() {
+    // Check if data is available in cache
+    if (this.head_list.length > 0) {
+      this.dataSource = new MatTableDataSource(this.head_list);
+      this.dataSource.paginator = this.paginator;
+      console.log('Loaded from cache');
+      return;
+    }
+    this.svr
+      .fin_getService('api/v0/get_secondary_heads')
+      .subscribe((res: any) => {
+        this.head_list = res;
+        this.dataSource = new MatTableDataSource(this.head_list);
+        this.dataSource.paginator = this.paginator;
+        console.log('Loaded from API');
+      });
+  }
 }
--- src/app/finance/modals/search-secondary-heads/search-secondary-heads.component.ts
+++ src/app/finance/modals/search-secondary-heads/search-secondary-heads.component.ts
@@ -3,70 +3,107 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ServiceService } from 'src/app/services/service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-search-secondary-heads',
   templateUrl: './search-secondary-heads.component.html',
-  styleUrl: './search-secondary-heads.component.scss'
+  styleUrl: './search-secondary-heads.component.scss',
 })
 export class SearchSecondaryHeadsComponent {
-
-
-
-
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  activeRowIndex: number | null = null;
+  selected_data: any;
+  rowColors: string[] = [];
+  head_list: any = [];
+  is_message: boolean = true;
   displayedColumns: string[] = ['type', 'code', 'head'];
-  dataSource = new MatTableDataSource<any>;
-  head_list: any;
+  dataSource = new MatTableDataSource<any>();
 
   // Modal footer buttons
   modalButtons = [
-
     {
       text: 'Cancel',
       className: 'btn btn-outline-primary-90 xs',
-      action: this.closeModal.bind(this)
+      action: this.closeModal.bind(this),
     },
     {
       text: 'Select',
       className: 'btn btn-primary-90 xs',
-      action: this.select_row.bind(this)
+      action: this.select_row.bind(this),
     },
   ];
 
-  constructor(private dialogRef: MatDialogRef<any>,
+  constructor(
+    private dialogRef: MatDialogRef<any>,
     private svr: ServiceService
-  ) { }
+  ) {}
+
   ngOnInit(): void {
-
     this.fetch_heads();
-
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   closeModal(): void {
     this.dialogRef.close({});
   }
 
+  rowActive(row: any, index: number) {
+    this.activeRowIndex = index;
+    this.selected_data = row;
+    console.log('Selected Data:', this.selected_data);
+    this.rowColors = this.rowColors.map(() => '');
+    this.rowColors[index] = '#ff0000';
+  }
   select_row() {
+    if (this.selected_data) {
+      Swal.fire({
+        icon: 'info',
+        title: 'Selected Record',
+        html: `
+          <p><strong>Type:</strong> ${this.selected_data.tny_type}</p>
+          <p><strong>Code:</strong> ${this.selected_data.vch_secondary_code}</p>
+          <p><strong>Head:</strong> ${this.selected_data.vch_secondary_head}</p>
+        `,
+      }).then(() => {
+        this.dialogRef.close({
+          result: 'selected data',
+          data: this.selected_data,
+        });
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Please Select A Record',
+      });
+    }
   }
 
+  // Fetch data from API
   fetch_heads() {
-    this.svr.fin_getService("api/v0/get_secondary_heads").subscribe((res: any) => {
-      this.head_list = res;
+    // Check if data is available in cache
+    if (this.head_list.length > 0) {
       this.dataSource = new MatTableDataSource(this.head_list);
       this.dataSource.paginator = this.paginator;
-
-    });
+      console.log('Loaded from cache');
+      return;
+    }
+    this.svr
+      .fin_getService('api/v0/get_secondary_heads')
+      .subscribe((res: any) => {
+        this.head_list = res;
+        this.dataSource = new MatTableDataSource(this.head_list);
+        this.dataSource.paginator = this.paginator;
+        console.log('Loaded from API');
+      });
   }
 
-
-    // for filter while search
-    applyFilter(event: Event) {
-      const filterValue = (event.target as HTMLInputElement).value;
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
-  
-
-
+  // for filter while search
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
