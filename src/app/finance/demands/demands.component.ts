6bdb3ba1d3b716ded07199f84d4da0bca7df28e5
arjuntechpool <arjun@techpool.co.in>
2025-04-19 10:57:44 +0530

    Inititalizing Demand Screen

--- src/app/finance/demands/demands.component.ts
+++ src/app/finance/demands/demands.component.ts
@@ -8,87 +8,134 @@ import { SearchApplicantComponent } from '../modals/search-applicant/search-appl
 import { SearchPropertyTaxComponent } from '../modals/search-property-tax/search-property-tax.component';
 
 @Component({
-  selector: 'app-demands', 
+  selector: 'app-demands',
   templateUrl: './demands.component.html',
-  styleUrl: './demands.component.scss'
+  styleUrl: './demands.component.scss',
 })
 export class DemandsComponent {
-
-  trn_list:any;
+  trn_list: any;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  displayedColumns: string[] = ['sl', 'headCode', 'head', 'year', 'period', 'amount'];
-  dataSource = new MatTableDataSource<any>;
+  displayedColumns: string[] = [
+    'sl',
+    'headCode',
+    'head',
+    'year',
+    'period',
+    'amount',
+  ];
+  dataSource = new MatTableDataSource<any>();
   selectedDate: Date | null = null;
-  selectedOption:any;
-  
+  selectedOption: any;
 
-  selectedType:any;
+  selectedType: any;
   instrument_options = [
-    { "id": 1, label: 'DD' },
-    { "id": 2, label: 'Cheque' },
+    { id: 1, label: 'DD' },
+    { id: 2, label: 'Cheque' },
   ];
   collection_options = [
-    { "id": 1, label: 'Direct Collection' },
-    { "id": 2, label: 'OutDoor Collection' },
+    { id: 1, label: 'Direct Collection' },
+    { id: 2, label: 'OutDoor Collection' },
   ];
 
-  selected_collection_option:any;
+  selected_collection_option: any;
 
   cash_mode: any;
-  
-  constructor( 
-       private dialog: MatDialog,
-  ){
 
-    
-    this.selectedOption =1;
-    this.cash_mode = "1";
-
+  constructor(private dialog: MatDialog) {
+    this.selectedOption = 1;
+    this.cash_mode = '1';
   }
 
-
-  ngOnInit(){
-    this.trn_list =[
-      { sl: 1, headCode: '101', head: 'Loreum Ipsum', year: 1020, period: 1020, amount: 1020 },
-      { sl: 2, headCode: '102', head: 'Loreum Ipsum', year: 420, period: 420, amount: 420 },
-      { sl: 3, headCode: '103', head: 'Loreum Ipsum', year: 140, period: 140, amount: 140 },
-      { sl: 4, headCode: '104', head: 'Loreum Ipsum', year: 70, period: 70, amount: 70 },
-      { sl: 5, headCode: '105', head: 'Loreum Ipsum', year: 1270, period: 1270, amount: 1270 },
-      { sl: 6, headCode: '106', head: 'Loreum Ipsum', year: 10020, period: 10020, amount: 10020 },
-      { sl: 7, headCode: '107', head: 'Loreum Ipsum', year: 6020, period: 6020, amount: 6020 },
-      { sl: 8, headCode: '107', head: 'Loreum Ipsum', year: 5020, period: 5020, amount: 5020 },
+  ngOnInit() {
+    this.trn_list = [
+      {
+        sl: 1,
+        headCode: '101',
+        head: 'Loreum Ipsum',
+        year: 1020,
+        period: 1020,
+        amount: 1020,
+      },
+      {
+        sl: 2,
+        headCode: '102',
+        head: 'Loreum Ipsum',
+        year: 420,
+        period: 420,
+        amount: 420,
+      },
+      {
+        sl: 3,
+        headCode: '103',
+        head: 'Loreum Ipsum',
+        year: 140,
+        period: 140,
+        amount: 140,
+      },
+      {
+        sl: 4,
+        headCode: '104',
+        head: 'Loreum Ipsum',
+        year: 70,
+        period: 70,
+        amount: 70,
+      },
+      {
+        sl: 5,
+        headCode: '105',
+        head: 'Loreum Ipsum',
+        year: 1270,
+        period: 1270,
+        amount: 1270,
+      },
+      {
+        sl: 6,
+        headCode: '106',
+        head: 'Loreum Ipsum',
+        year: 10020,
+        period: 10020,
+        amount: 10020,
+      },
+      {
+        sl: 7,
+        headCode: '107',
+        head: 'Loreum Ipsum',
+        year: 6020,
+        period: 6020,
+        amount: 6020,
+      },
+      {
+        sl: 8,
+        headCode: '107',
+        head: 'Loreum Ipsum',
+        year: 5020,
+        period: 5020,
+        amount: 5020,
+      },
     ];
 
     this.dataSource = new MatTableDataSource(this.trn_list);
-    
-
   }
 
-  fetch_trn_list(){
-
-  }
-
+  fetch_trn_list() {}
 
   onOptionChange(value: any): void {
     console.log(value);
-    
+
     this.selectedOption = value;
-    
   }
 
-  
   open_transactions() {
     const dialogRef = this.dialog.open(SearchTranactionTypesComponent, {
       width: '1130px',
     });
     dialogRef?.afterClosed().subscribe((response: any) => {
       if (response && response.data) {
-        if(response.data.id.toString()=="2") {
+        if (response.data.id.toString() == '2') {
           this.search_property_tax();
         }
         console.log(response);
-        
       }
     });
   }
@@ -103,7 +150,7 @@ export class DemandsComponent {
     });
   }
 
-  Search_applicant(){
+  Search_applicant() {
     const dialogRef = this.dialog.open(SearchApplicantComponent, {
       width: '1130px',
     });
@@ -113,7 +160,7 @@ export class DemandsComponent {
     });
   }
 
-  search_property_tax(){
+  search_property_tax() {
     const dialogRef = this.dialog.open(SearchPropertyTaxComponent, {
       width: '1130px',
     });
@@ -121,7 +168,5 @@ export class DemandsComponent {
       if (response && response.data) {
       }
     });
-  
-   }
-
+  }
 }
